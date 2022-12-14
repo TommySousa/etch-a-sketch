3f243a449f5a397e15cbd919bf04cb0a7756f988
Tomás Sousa <118855300+TommySousa@users.noreply.github>
2022-12-14 13:05:46 +0000

    Added the hovering paint functionality

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ for (let i = 0; i<16; i++){
         div.appendChild(divs);
     }
 };
+
+//MAKING THE CLASS HOVER ACTIVE WHEN THE MOUSE HOOVERS IN THE HTML SQUARES
+const hover = document.getElementsByClassName('horizontal');
+for (let i = 0; i < hover.length; i++){
+     hover[i].addEventListener('mouseover', (el) => {
+        el.target.classList.add('horizontal_hover')
+     })
+ };
+
